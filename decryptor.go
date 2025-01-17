00ab9a3c61af23595599bdaa9ad2ca455f0f1ce6
Simon Krenger <skrenger@redhat.com>
2025-01-17 16:11:20 +0100

    Add functionality to read encrypted data from file (#3)
    This change adds the functionality that the encrypted data from etcd can be read from a file.
    
    In practice we have seen that the tool can fail with the following error message when a very large object (for example a large `Secret`) is pasted as the input:
    
    ```
    Failed to decode etcd value: illegal base64 data at input byte 4093
    ```
    
    It turns out that this is due to the terminal input buffer size limit, which is 4096 bytes on
    GNU/Linux (look for N_TTY_BUF_SIZE in the kernel sources): https://groups.google.com/g/golang-nuts/c/ndh-1wdsWYs/m/Watbhx8JAwAJ
    
    When such a case is detected, then a message is printed and the user should put his values in a file called `secretvalue`. The program will then read from that file if it is present and skip asking for the encrypted data.
    
    The change also updates the README with the current location of the EncryptionConfig in newer OCP versions.

--- decryptor.go
+++ decryptor.go
@@ -5,6 +5,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -13,17 +14,41 @@ import (
 	aestransformer "k8s.io/apiserver/pkg/storage/value/encrypt/aes"
 )
 
+const etcdValueFile string = "secretvalue"
+
 func main() {
 	fmt.Println("Tool to decrypt AES-CBC-encrypted objects from etcd")
 
-	fmt.Print("Enter base64-encoded etcd value: ")
-	reader := bufio.NewReader(os.Stdin)
-	b, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Printf("Error reading input: %v", err)
-		os.Exit(1)
+	var secretString string
+	if _, err := os.Stat(etcdValueFile); errors.Is(err, os.ErrNotExist) {
+		// There is no file with the secret value, read from stdin
+		fmt.Print("Enter base64-encoded etcd value: ")
+		reader := bufio.NewReader(os.Stdin)
+		secretString, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Error reading input: %v", err)
+			os.Exit(1)
+		}
+		// Check if we possibly hit the terminal input buffer size limit
+		// https://groups.google.com/g/golang-nuts/c/ndh-1wdsWYs/m/Watbhx8JAwAJ
+		if len(secretString) > 4095 {
+			fmt.Println("The string you entered is longer than 4095 bytes.")
+			fmt.Println("When running this in a terminal, you are likely hitting the terminal input buffer size limit.")
+			fmt.Printf("Please write your encrypted etcd value in a file called '%s' and run this program again.", etcdValueFile)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Println("Found file with secret value, reading from the file...")
+		// File exists, read from file
+		b, err := os.ReadFile(etcdValueFile)
+		if err != nil {
+			fmt.Printf("Error reading input from file: %v", err)
+			os.Exit(1)
+		}
+		secretString = string(b)
 	}
-	v, err := base64.StdEncoding.DecodeString(b)
+
+	v, err := base64.StdEncoding.DecodeString(secretString)
 	if err != nil {
 		fmt.Printf("Failed to decode etcd value: %v\n", err)
 		os.Exit(1)
@@ -31,14 +56,16 @@ func main() {
 
 	// Decoded string looks like this: "k8s:enc:aescbc:v1:<provider-name>:<binary-aes-encrypted-data>"
 	// "<binary-aes-encrypted-data>" := "<32-bit IV><rest-of-data>"
-	s := strings.Split(string(v), ":")
+	s := strings.SplitN(string(v), ":", 6)
 
 	if len(s) != 6 {
 		fmt.Printf("Value does not have the right format: %v", s)
+		fmt.Println("Length of array is", len(s))
 		os.Exit(1)
 	}
 	if s[2] != "aescbc" {
 		fmt.Printf("Secret is not CBC-encrypted: %v\n", s[2])
+		fmt.Println("This tool currently only supports AES-CBC encrypted secrets")
 		os.Exit(1)
 	}
 
@@ -46,7 +73,7 @@ func main() {
 	secret := []byte(s[5])
 
 	fmt.Print("Enter base64-encoded encryption key from EncryptionConfig: ")
-	reader = bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 	key, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Printf("Error reading key: %v\n", err)
